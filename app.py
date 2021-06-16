98b9fe4ea19daab4ee82fc62e80ebfa3183a9aff
Taraxacum01 <71626603+Taraxacum01@users.noreply.github.com>
2021-06-16 17:33:09 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask
 from config import Configuration
-from db.database import Database
-
-DATABASE = 'posts.db'
-
-db = Database(DATABASE)
-db.init_db()
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
--- view.py
+++ view.py
@@ -1,6 +1,6 @@
-from app import app, db
-from flask import render_template
-
+from app import app
+from flask import render_template, redirect
+from users.user_verification import check_user, add_user
 
 @app.route('/')
 def index():
@@ -8,10 +8,20 @@ def index():
 
 @app.route('/main')
 def main():
-    posts = db.get_posts()
-    posts = [dict(post) for post in posts]
-    return render_template('index.html', posts=posts)
+    return render_template('main.html')
 
 @app.route('/user_verification')
 def user_verification():
     return render_template('user_verification.html')
+
+@app.route('/log_in', methods=['post'])
+def log_in():
+    
+    return redirect('/main')
+    
+
+@app.route('/registration', methods=['post'])
+def registration():
+    if(not check_user()):
+        add_user()
+    return redirect('/main')
