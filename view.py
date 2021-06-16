dd7fffccc95513f03fe076939aa7e576e95e34de
Taraxacum01 <71626603+Taraxacum01@users.noreply.github.com>
2021-06-16 14:11:50 +1000

    Add files via upload

--- view.py
+++ view.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/user_verification')
+def user_verification():
+    return render_template('user_verification.html')
