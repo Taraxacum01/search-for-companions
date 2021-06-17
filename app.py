f8bb80a0aaa8492e2b41b7399450c7cf041aaae8
GoSum215 <85661196+GoSum215@users.noreply.github.com>
2021-06-17 14:42:51 +1000

    New grouping

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ from config import Configuration
 from db.database import Database
 from users.blueprint import users
 
-DATABASE = 'posts.db'
-
-db = Database(DATABASE)
-db.init_db()
-
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
