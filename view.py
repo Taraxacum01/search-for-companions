6ba2e940f2d1990cb9dacfc0c76dd6e815309948
GoSum215 <85661196+GoSum215@users.noreply.github.com>
2021-06-17 14:45:49 +1000

    + page for each post

--- view.py
+++ view.py
@@ -1,4 +1,5 @@
-from app import app, db
+from app import app
+from config import db
 from flask import render_template, redirect
 
 @app.route('/')
@@ -7,6 +8,10 @@ def index():
 
 @app.route('/main')
 def main():
-    posts = db.get_posts()
-    posts = [dict(post) for post in posts]
-    return render_template('main.html', posts=posts)
+    return render_template('main.html', posts=db.get_posts())
+
+@app.route('/posts/<int:id>')
+def about(id):
+    if id not in range(1, len(db.get_posts()) + 1):
+        return "", 404
+    return render_template('post.html', post=db.get_post(id))
